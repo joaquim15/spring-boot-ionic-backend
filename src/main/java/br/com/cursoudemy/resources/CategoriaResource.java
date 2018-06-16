b40b79123aed09c882a52ed30623f45cc257df36
Joaquim de Castro Moura <joaquim_castro_moura@hotmail.com>
2018-06-16 14:07:49 -0300

    Criação da classe de dominio lista de Categorias

--- src/main/java/br/com/cursoudemy/resources/CategoriaResource.java
+++ src/main/java/br/com/cursoudemy/resources/CategoriaResource.java
@@ -1,7 +1,13 @@
 package br.com.cursoudemy.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cursoudemy.domain.Categoria;
+
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
@@ -9,8 +15,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String Listar() {
-		return "REST está funcionando";
+	public List<Categoria> Listar() {
+
+		Categoria cat1 = new Categoria(1L, "Informatica");
+		Categoria cat2 = new Categoria(2L, "Escritório");
+
+		List<Categoria> listCategorias = new ArrayList<>();
+
+		listCategorias.add(cat1);
+		listCategorias.add(cat2);
+
+		return listCategorias;
 	}
 
 }
