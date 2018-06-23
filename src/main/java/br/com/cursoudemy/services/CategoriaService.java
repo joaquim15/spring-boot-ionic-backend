bcb957f573c3cfb72f1eeb0e5191bd7a3bc6c279
Joaquim de Castro Moura <joaquim_castro_moura@hotmail.com>
2018-06-23 16:19:19 -0300

    Paginação com paramentros opicionais na Requisição.

--- src/main/java/br/com/cursoudemy/services/CategoriaService.java
+++ src/main/java/br/com/cursoudemy/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.cursoudemy.domain.Categoria;
@@ -49,5 +52,13 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		// TODO Auto-generated method stub
 		return this.repository.findAll();
+	}
+
+	@SuppressWarnings("deprecation")
+	public Page<Categoria> findPage(Integer page, Integer linesPorPage, String orderBy, String direction) {
+
+		PageRequest pageRequest = new PageRequest(page, linesPorPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+
 	}
 }
