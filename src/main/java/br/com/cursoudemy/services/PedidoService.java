7f2d462dd9b43f13cc2b9a147af4865cc0cc4142
Joaquim de Castro Moura <joaquim_castro_moura@hotmail.com>
2018-06-27 10:00:14 -0300

    Implementando toString de pedido.

--- src/main/java/br/com/cursoudemy/services/PedidoService.java
+++ src/main/java/br/com/cursoudemy/services/PedidoService.java
@@ -14,7 +14,6 @@ import br.com.cursoudemy.domain.enums.EstadoPagamento;
 import br.com.cursoudemy.repositories.ItemPedidoRepository;
 import br.com.cursoudemy.repositories.PagamentoRepository;
 import br.com.cursoudemy.repositories.PedidoRepository;
-import br.com.cursoudemy.repositories.ProdutoRepository;
 import br.com.cursoudemy.services.exceptions.ObjectNotFoundExeption;
 
 @Service
@@ -25,15 +24,18 @@ public class PedidoService {
 
 	@Autowired
 	private BoletoService boletoService;
-
+	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
 	
 	@Autowired
-	private ProdutoRepository produtoRepository;
+	private ItemPedidoRepository pedidoRepository;
 	
 	@Autowired
-	private ItemPedidoRepository pedidoRepository;
+	private ClienteService clienteService;
+	
+	@Autowired
+	private ProdutoService produtoService;
 
 	public Pedido find(Integer id) {
 
@@ -47,6 +49,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstance(new Date());
+		obj.setCliente(this.clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 
@@ -59,11 +62,12 @@ public class PedidoService {
 		this.pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(this.produtoRepository.findById(ip.getProduto().getId()).get().getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		this.pedidoRepository.saveAll(obj.getItens());
-		
+		System.out.println(obj);
 		return obj;
 	}
 }
